8ee5946b248ace381072fce4a2e9771daf071f0d
kifel <joaovictorm.pessoal@gmail.com>
2022-05-03 12:55:55 -0300

    Create menu mobile

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ function onScroll (){
         navigation.classList.remove('scroll')
     }
 }
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
