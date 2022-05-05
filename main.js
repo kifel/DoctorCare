bef21aaccee7c41bff40d17ea5b31cabb53c041a
kifel <joaovictorm.pessoal@gmail.com>
2022-05-05 17:23:47 -0300

    Back to home button and custom colors

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-function onScroll (){
+window.addEventListener('scroll', onScroll)
 
+onScroll()
+function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
     if (scrollY > 0) {
         navigation.classList.add('scroll')
     } else {
         navigation.classList.remove('scroll')
+    }
+}
+
+function showBackToTopButtonOnScroll() {
+    if (scrollY > 400) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
     }
 }
 
